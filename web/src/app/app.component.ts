89ff2d7102cf4d231d470dd446623250779b6867
Igor Tessaro <igorstessaro57@gmail.com>
2023-11-07 08:16:00 -0300

    feat(login): refactor navbar and add user login functionality

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Observable } from 'rxjs';
 import { AuthService } from './services/auth.service';
+import { UserLogin } from './models/user-login';
 
 @Component({
     selector: 'app-root',
@@ -9,17 +10,22 @@ import { AuthService } from './services/auth.service';
     styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-    title = 'descomplica-gastos-web';
-    isLoggedIn$: Observable<boolean>;
+    public title = 'descomplica-gastos-web';
+    public isLoggedIn$: Observable<boolean>;
+    public loggedUser$: Observable<UserLogin | null>;
 
     constructor(private modalService: NgbModal, private authService: AuthService) {
         this.isLoggedIn$ = this.authService.isLoggedIn;
+        this.loggedUser$ = this.authService.loggedUser;
     }
 
-    ngOnInit(): void {
-    }
+    ngOnInit(): void {}
 
     public open(modal: any): void {
         this.modalService.open(modal);
+    }
+
+    public logout(): void {
+        this.authService.logout();
     }
 }
